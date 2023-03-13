cdb1eebcf8bdd1aaca128088008ce3d9290b6403
dashanteCEO <deshantestreet749@gmail.com>
2023-03-13 19:06:51 +0000

    d

--- server.js
+++ server.js
@@ -22,14 +22,6 @@ const DB = async () => {
   }
 };
 DB()
-app.use(cors(
-  {
-  //fix preflight error
-  credentials: true,
-  // allowedHeaders: ["Content-Type", "Authorization", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
-  'Access-Control-Allow-Origin' :'https://ddautoja-backend-production.up.railway.app'
-  }
-));
 app.use(mongoSanitize());
 app.use(morgan("dev"));
 app.use(xss());
