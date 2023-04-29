d4d3501fe9d1c0df6643aaa4315ba8de8a0a2195
streetetech <76850645+streetetech@users.noreply.github.com>
2023-04-29 04:39:34 +0000

    Update server.js fc

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ const DB = async () => {
 DB()
 app.use(cors(
   {
-  //fix preflight error
-    orgin:'https://ddauto.up.railway.app',
+   orgin:'https://ddauto.up.railway.app',
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
 'Access-Control-Allow-Origin' : 'localhost:3000'
