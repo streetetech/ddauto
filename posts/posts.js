294a6cd4b8a885a73553c18dcb6ef0dbe3b69c0c
streetetech <76850645+streetetech@users.noreply.github.com>
2023-05-02 22:52:23 +0000

    Update posts.js

--- posts/posts.js
+++ posts/posts.js
@@ -84,41 +84,83 @@ router.get("/assets/:filename", (req, res) => {
   });
 });
 
+
 router.get("/search/:brand", async (req, res) => {
   try {
     const brand = req.params.brand; // Get the brand from the URL params
-    const files = await gfs.find({ "metadata.brand": brand }).toArray();
-    // Do files exist for the brand?
+    const files = await gfs.find({ "metadata.brand": brand }).toArray();    // Does file exist?
     if (!files || files.length === 0) {
-      return res.status(404).send("No files found for the given brand");
+      return res.status(404).send("No files found");
     }
 
-  const urls = files.map((file) =>{
-console.log(file.metadata)
+    const groups = {};
 
-     return {
-      url:`https://ddauto.up.railway.app/api/post/assets/${file.filename}`,
-      brand: file.metadata.brand,
-      color:file.metadata.color,
-      model: file.metadata.model,
-      year: file.metadata.year,
-      bodyType: file.metadata.bodyType,
-      specs: file.metadata.specs,
-      mileage : file.metadata.mileage,
-      seats : file.metadata.seats,
-      feul : file.metadata.feul,
-      transmission : file.metadata.transmission,
-      steering : file.metadata.steering,
-      price : file.metadata.price,
-    }
-  });
+    files.forEach((file) => {
+      if (file.metadata && file.metadata.groupId) {
+        // check if any metadata field is null
+        const metadataValues = Object.values(file.metadata);
+        if (metadataValues.some((value) => value === null)) {
+          return; // skip this file
+        }
+        const groupId = file.metadata.groupId;
+        const brand = file.metadata.brand;
+        if (!groups[groupId] || groups[groupId].brand === brand) {
+          const model = file.metadata.model;
+          const color = file.metadata.color;
+          const year = file.metadata.year;
+          const bodyType = file.metadata.bodyType;
+          const specs = file.metadata.specs;
+          const mileage = file.metadata.mileage
+          const seats = file.metadata.seats
+          const feul = file.metadata.feul
+          const transmission = file.metadata.transmission
+          const steering = file.metadata.steering
+          const price = file.metadata.price
 
-    res.send({ urls });
+          groups[groupId] = {
+            url: `https://ddauto.up.railway.app/api/post/assets/${file.filename}`,
+            brand: brand,
+            model: model,
+            year: year,
+            color: color,
+            specs: specs,
+            bodyType: bodyType,
+            mileage: mileage,
+            feul:feul,
+            steering: steering,
+            transmission: transmission,
+            seats:seats,
+            price:price
+          };
+        }
+      }
+    });
+
+    const urls = Object.entries(groups).map(([groupId, data]) => ({
+      groupId,
+      url: data.url,
+      brand: data.brand,
+      year: data.year,
+      color: data.color,
+      bodyType: data.bodyType,
+      model: data.model,
+      specs: data.specs,
+      mileage: data.mileage,
+      feul: data.feul,
+      seats: data.seats,
+      transmission: data.transmission,
+      steering: data.steering,
+      price: data.price,
+    }));
+
+    res.send({ urls })
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error");
   }
 });
+
+
 
 router.delete("/delete/:groupId", async (req, res) => {
   try {
