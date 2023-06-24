90c2c4b7ffc7da2fe02a1170d57aac1b50119e18
streetetech <76850645+streetetech@users.noreply.github.com>
2023-06-24 00:11:50 +0000

    Update posts.js

--- posts/posts.js
+++ posts/posts.js
@@ -411,60 +411,76 @@ router.get("/search/:brand", async (req, res) => {
   }
 });
 
-
-router.get("/search/:brand/:model", async (req, res) => {
+router.get("/searchretbrand/:brand", async (req, res) => {
   try {
     const brand = req.params.brand; // Get the brand from the URL params
-    const model = req.params.model; // Get the model from the URL params
-
+    const page = parseInt(req.query.page) || 1; // Get the page number from the query parameter, default to 1 if not provided
     const files = await gfs
-      .find({ "metadata.brand": brand, "metadata.model": model })
-      .toArray(); // Find files matching the brand and model
+      .find({ "metadata.brand": brand })
+      .toArray();
+
+    // Does file exist?
     if (!files || files.length === 0) {
       return res.status(404).send("No files found");
     }
 
-    const vehicles = files.map((file) => {
-      const groupId = file.metadata.groupId;
-      const brand = file.metadata.brand;
-      const model = file.metadata.model;
-      const year = file.metadata.year;
-      const color = file.metadata.color;
-      const specs = file.metadata.specs;
-      const bodyType = file.metadata.bodyType;
-      const mileage = file.metadata.mileage;
-      const feul = file.metadata.feul;
-      const steering = file.metadata.steering;
-      const transmission = file.metadata.transmission;
-      const seats = file.metadata.seats;
-      const price = file.metadata.price;
-      const trim = file.metadata.trim;
+    const groups = {};
 
-      return {
-        groupId: groupId,
-        url: `https://ddauto.up.railway.app/api/post/assets/${file.filename}`,
-        brand: brand,
-        model: model,
-        year: year,
-        color: color,
-        specs: specs,
-        bodyType: bodyType,
-        mileage: mileage,
-        feul: feul,
-        steering: steering,
-        transmission: transmission,
-        seats: seats,
-        price: price,
-        trim: trim,
-      };
+    files.forEach((file) => {
+      if (file.metadata && file.metadata.groupId) {
+        // check if any metadata field is null
+        const metadataValues = Object.values(file.metadata);
+        if (metadataValues.some((value) => value === null)) {
+          return; // skip this file
+        }
+        const groupId = file.metadata.groupId;
+        const brand = file.metadata.brand;
+        const model = file.metadata.model;
+        const color = file.metadata.color;
+        const year = file.metadata.year;
+        const bodyType = file.metadata.bodyType;
+        const specs = file.metadata.specs;
+        const mileage = file.metadata.mileage;
+        const seats = file.metadata.seats;
+        const feul = file.metadata.feul;
+        const transmission = file.metadata.transmission;
+        const steering = file.metadata.steering;
+        const price = file.metadata.price;
+        const trim = file.metadata.trim;
+        if (!groups[groupId]) {
+          groups[groupId] = {
+            url: `https://ddauto.up.railway.app/api/post/assets/${file.filename}`,
+            brand: brand,
+            model: model,
+            year: year,
+            color: color,
+            specs: specs,
+            bodyType: bodyType,
+            mileage: mileage,
+            feul: feul,
+            steering: steering,
+            transmission: transmission,
+            seats: seats,
+            price: price,
+            trim: trim,
+          };
+        }
+      }
     });
 
-    res.send({ vehicles });
+    const ITEMS_PER_PAGE = 10;
+    const startIndex = (page - 1) * ITEMS_PER_PAGE;
+    const endIndex = page * ITEMS_PER_PAGE;
+    const paginatedUrls = Object.values(groups)
+      .slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(Object.keys(groups).length / ITEMS_PER_PAGE);
+    res.send({ urls: paginatedUrls, totalPages });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error");
   }
 });
 
-
+  
 module.exports = router;
